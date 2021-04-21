1bf776d3117d7e9a18118eb6d437ee9c3da677c8
elbeicktalat <talatelbeick@gmail.com>
2021-04-21 21:41:41 +0200

    add start and end with

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,5 @@
 import 'package:data_faker/data_faker.dart' show Name;
 
 void main() {
-  print(Name(charCount: 4).name);
+  print(Name(charCount: 6, startWith: 'H', endWith: 'y').name);
 }
--- lib/src/name.dart
+++ lib/src/name.dart
@@ -4,6 +4,8 @@ import 'package:data_faker/data/data.dart' as data show names;
 class Name {
   Name({
     this.charCount,
+    this.startWith = '',
+    this.endWith = '',
   }) {
     try {
       getName();
@@ -12,13 +14,18 @@ class Name {
 
   final int? charCount;
   late String? name;
+  final String? startWith;
+  final String? endWith;
+
   final List<String> _names = [];
   final Random _random = Random();
 
   String getName() {
     data.names.forEach((name) {
       if (name.length == charCount) {
-        _names.add(name);
+        if (name.startsWith(startWith!) && name.endsWith(endWith!)) {
+            _names.add(name);
+          }
       }
     });
     return this.name = _names.elementAt(_random.nextInt(_names.length));
