55e662461fafaad26e8a9be3d68cd0327ca27bc1
elbeicktalat <talatelbeick@gmail.com>
2021-04-21 21:55:36 +0200

    add text cases

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,10 @@
-import 'package:data_faker/data_faker.dart' show Name;
+import 'package:data_faker/data_faker.dart';
 
 void main() {
-  print(Name(charCount: 6, startWith: 'H', endWith: 'y').name);
+  print(Name(
+    charCount: 6,
+    startWith: 'H',
+    endWith: 'y',
+    textCase: TextCase.lower,
+  ).name);
 }
--- lib/src/name.dart
+++ lib/src/name.dart
@@ -1,11 +1,17 @@
 import 'dart:math';
 import 'package:data_faker/data/data.dart' as data show names;
 
+enum TextCase {
+  upper,
+  lower,
+}
+
 class Name {
   Name({
     this.charCount,
     this.startWith = '',
     this.endWith = '',
+    this.textCase,
   }) {
     try {
       getName();
@@ -16,7 +22,7 @@ class Name {
   late String? name;
   final String? startWith;
   final String? endWith;
-
+  final TextCase? textCase;
   final List<String> _names = [];
   final Random _random = Random();
 
@@ -24,8 +30,17 @@ class Name {
     data.names.forEach((name) {
       if (name.length == charCount) {
         if (name.startsWith(startWith!) && name.endsWith(endWith!)) {
-            _names.add(name);
+          switch (textCase) {
+            case TextCase.upper:
+              _names.add(name.toUpperCase());
+              break;
+            case TextCase.lower:
+              _names.add(name.toLowerCase());
+              break;
+            default:
+              _names.add(name);
           }
+        }
       }
     });
     return this.name = _names.elementAt(_random.nextInt(_names.length));
