7eed51b48dffc4ea2e7f53aa1bc1e9222b07eb07
elbeicktalat <talatelbeick@gmail.com>
2021-04-22 21:41:55 +0200

    add @ for the email domains

--- lib/data/res/email_domain.dart
+++ lib/data/res/email_domain.dart
@@ -1,8 +1,8 @@
 List<String> emailDomains = [
-  'gmail.com',
-  'yahoo.com',
-  'outlook.com',
-  'hotmail.com',
-  'aol.com',
-  'live.com',
+  '@gmail.com',
+  '@yahoo.com',
+  '@outlook.com',
+  '@hotmail.com',
+  '@aol.com',
+  '@live.com',
 ];
