88d481342e860dce3e299fd7d8ee43d7e0d1de7f
elbeicktalat <talatelbeick@gmail.com>
2021-04-25 17:48:52 +0200

    add random generator

---
+++ lib/src/random.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+class RandomGenerator {
+  RandomGenerator({int? seed}) : _random = Random(seed);
+
+  final Random _random;
+
+  /// Gets a random element from the given [list].
+  T element<T>(List<T> list) {
+    return list[_random.nextInt(list.length)];
+  }
+}
