350f1951b4b6645d9b4e8c72591322f439a61594
elbeicktalat <talatelbeick@gmail.com>
2021-04-25 17:16:32 +0200

    export email

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,4 @@
 import 'package:data_faker/data_faker.dart';
-import 'package:data_faker/src/email.dart';
 
 void main() {
   print(Name(charCount: 3, gender: Gender.male).name);
--- lib/data_faker.dart
+++ lib/data_faker.dart
@@ -1,5 +1,6 @@
 library data_faker;
 
 export 'package:data_faker/src/name.dart';
+export 'package:data_faker/src/email.dart';
 export 'package:data_faker/utils/text_case.dart';
 export 'package:data_faker/utils/gender.dart';
